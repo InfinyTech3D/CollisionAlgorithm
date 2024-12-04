a913098c3f94351cda30c847bd252b4677ccb040
Hadrien Courtecuisse <hadrien.courtecuisse@gmail.com>
2024-12-04 17:42:26 +0100

    "doc"

--- src/sofa/collisionAlgorithm/broadphase/AABBBroadPhase.cpp
+++ src/sofa/collisionAlgorithm/broadphase/AABBBroadPhase.cpp
@@ -3,7 +3,11 @@
 
 namespace sofa::collisionAlgorithm {
 
-int AABBBroadPhaseClass = core::RegisterObject("AABBBroadPhase")
+int AABBBroadPhaseClass = core::RegisterObject(R"(
+**AABBBroadPhase** is a real-time collision detection component that uses the Axis-Aligned Bounding Box (AABB) approach for broad-phase collision handling.
+
+It organizes elements into a 3D spatial grid, efficiently indexing and retrieving them to identify potential collisions, optimizing the process of narrowing down collision checks for more precise detection in 3D simulations."
+)")
 .add< AABBBroadPhase >();
 
 }
