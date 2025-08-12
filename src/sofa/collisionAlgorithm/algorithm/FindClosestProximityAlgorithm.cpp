00a763a002ff118300f736ea64ea6c6646f6bc00
Themis Skamagkis <themisskamagkis+github@gmail.com>
2025-08-12 22:08:37 +0200

    [src][algorithm] Apply new factory registration mechanism

--- src/sofa/collisionAlgorithm/algorithm/FindClosestProximityAlgorithm.cpp
+++ src/sofa/collisionAlgorithm/algorithm/FindClosestProximityAlgorithm.cpp
@@ -1,13 +1,13 @@
 #include <sofa/collisionAlgorithm/algorithm/FindClosestProximityAlgorithm.h>
 #include <sofa/core/ObjectFactory.h>
 
-namespace sofa::collisionAlgorithm {
-
-int FindClosestProximityAlgorithmClass = core::RegisterObject("FindClosestProximityAlgorithm")
-.add< FindClosestProximityAlgorithm >();
-
-
+namespace sofa::collisionAlgorithm
+{
+void registerFindClosestProximityAlgorithm(sofa::core::ObjectFactory* factory)
+{
+    factory->registerObjects(
+        sofa::core::ObjectRegistrationData(
+            "An algorithm to find the closest proximity between two BaseGeometry types")
+            .add<FindClosestProximityAlgorithm>());
 }
-
-
-
+}  // namespace sofa::collisionAlgorithm
--- src/sofa/collisionAlgorithm/algorithm/InsertionAlgorithm.cpp
+++ src/sofa/collisionAlgorithm/algorithm/InsertionAlgorithm.cpp
@@ -1,13 +1,12 @@
 #include <sofa/collisionAlgorithm/algorithm/InsertionAlgorithm.h>
 #include <sofa/core/ObjectFactory.h>
 
-namespace sofa::collisionAlgorithm {
-
-int InsertionAlgorithmClass = core::RegisterObject("InsertionAlgorithm")
-.add< InsertionAlgorithm >();
-
-
+namespace sofa::collisionAlgorithm
+{
+void registerInsertionAlgorithm(sofa::core::ObjectFactory* factory)
+{
+    factory->registerObjects(sofa::core::ObjectRegistrationData(
+                                 "A class implementing a customized needle insertion algorithm")
+                                 .add<InsertionAlgorithm>());
 }
-
-
-
+}  // namespace sofa::collisionAlgorithm
